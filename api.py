504c78d2fb2edccdf79a81add952618cc4e7ebf9
MeesalaGopichand <45434440+MeesalaGopichand@users.noreply.github.com>
2023-10-11 07:11:22 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,46 +4,66 @@ app = Flask(__name__)
 
 @app.route("/get-status/<service_tag>")
 def get_status(service_tag):
-    device_data = {}
-    if(service_tag == "asdf"):
-        device_data = {
-            "name" : "power edge server x40",
-            "type" : "server",
-            "status" : "expired",
-            "exp_date" : "12-02-2022",
-            "email" : "user@dell.com",
-            "service_tag" : service_tag,
-            "r2years" : "8000",
-            "r3years" : "11000"
-        }
-    if(service_tag == "asd"):
-        device_data = {
-            "name" : "power edge server 123",
-            "type" : "server",
-            "status" : "active",
-            "exp_date" : "10-01-2023",
-            "email" : "user@dell.com",
-            "service_tag" : service_tag,
-        }
-    if(service_tag == "asdfg"):
-        device_data = {
-            "name" : "latitude 5530",
-            "type" : "laptop",
-            "status" : "active",
-            "exp_date" : "15-10-2023",
-            "email" : "user@dell.com",
-            "service_tag" : service_tag,
-            "r2years" : "2000",
-            "r3years" : "2500"
-         }
+    if(service_tag != ""):
+        device_data = {}
+        status_code = 200
+        if(service_tag == "54FZTT2"):
+            device_data = {
+                "name" : "Latitude 5490",
+                "type" : "server",
+                "status" : "Expired",
+                "exp_date" : "07 FEB 2022",
+                "email" : "user@dell.com",
+                "service_tag" : service_tag,
+                "n_status" : "0",
+                "p_url" : "https://www.dell.com/support/home/en-in/product-support/servicetag/"+service_tag,
+                "img_url" : "latitude5490.jpg"
+            }
+        elif(service_tag == "4YXMQN2"):
+            device_data = {
+                "name" : "Latitude 5480/5488",
+                "type" : "server",
+                "status" : "Expired",
+                "exp_date" : "27 APR 2021",
+                "email" : "user@dell.com",
+                "service_tag" : service_tag,
+                "n_status" : "0",
+                "p_url" : "https://www.dell.com/support/home/en-in/product-support/servicetag/"+service_tag,
+                "img_url" : "latitude5480.jpg"
+            }
+        elif(service_tag == "3C1ZTL3"):
+            device_data = {
+                "name" : "Latitude 5530",
+                "type" : "laptop",
+                "status" : "Active",
+                "exp_date" : "27 AUG 2025",
+                "email" : "user@dell.com",
+                "service_tag" : service_tag,
+                "n_status" : "2",
+                "p_url" : "https://www.dell.com/support/home/en-in/product-support/servicetag/"+service_tag,
+                "img_url" : "latitude5530.jpg"
+            }
+        else:
+            device_data = {
+                "error" : " no device found",
+                "service_tag" : service_tag,
+                "n_status" : "3"
+            }
+            status_code = 200
 
-    api_key = request.args.get("api_key")
-    if api_key:
-        device_data['api_key'] = api_key
+        res = make_response(jsonify(device_data), status_code)
+        res.headers['Access-Control-Allow-Origin'] = "*"
+        return res
+    # except:
+    #     device_data = {
+    #         "error" : " no device found",
+    #         "service_tag" : service_tag,
+    #         "n_status" : "3"
+    #     }
+    #     res = make_response(jsonify(device_data), 200)
+    #     res.headers['Access-Control-Allow-Origin'] = "*"
+    #     return res
 
-    res = make_response(jsonify(device_data), 200)
-    res.headers['Access-Control-Allow-Origin'] = "*"
-    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
